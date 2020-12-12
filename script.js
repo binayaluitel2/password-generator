e4e04b2573541ce71cdd6702c9b23cfc7346d212
binayaluitel2 <binayababuluitel@gmail.com>
2020-12-12 08:24:06 -0500

    Added condition to restrict between 8 and 128

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+	     var pwdlength = prompt("Please enter length of password between 8 and 128");
 
-   var password = 'password';
+     pwdlength=parseInt(pwdlength);
+
+     if (pwdlength < 8 || pwdlength > 128) {
+     		     var pwdlength = alert("Please reclick 'Generate Password' and re-enter length of password between 8 and 128");
+
+     }
+
+   var password = 'passwordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpassword';
 
 
   return password;
