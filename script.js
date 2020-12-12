4345dbfe2c482ac1177015ca8660f81968c06b4a
binayaluitel2 <binayababuluitel@gmail.com>
2020-12-12 07:47:18 -0500

    Initial Commit - Placeholder

---
+++ script.js
@@ -0,0 +1,29 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+   var password = 'password';
+
+
+  return password;
+
+}
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+    // var password = "generatePassword";
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
